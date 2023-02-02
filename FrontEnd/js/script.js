d7c948618c827f2ccf82354a746874d6a1f54d73
Albéric Andreae <andreaealberic@gmail.com>
2023-02-02 11:13:29 +0100

    Ajout fonctionnalités JS
    Ajout des deux modales
    Ajout de la fonctionnalité POST de projet (non fonctionnelle)

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -88,85 +88,255 @@ function filtreCat(filtresType) {
       projet.style.display = "block";
     }
   }
-  
 
   // afficherProjets(nouveauTab);
 }
 
 // LOGGED-IN ------------------------------------------------------------------
 
-function login(token){
-  if (token){
-  document.getElementById("account").innerText = "logout";
-  } else{
+function login(token) {
+  if (token) {
+    document.getElementById("account").innerText = "logout";
+  } else {
     document.getElementById("account").innerText = "login";
   }
 }
 login(token);
 
-function deco(token){
-  if (token){
+function deco(token) {
+  if (token) {
     localStorage.removeItem("token");
   }
 }
 const logout = document.getElementById("account");
-logout.addEventListener('click', deco);
+logout.addEventListener("click", deco);
 
-
-// Déclaration variables de la page édition (utilisateur log)------------------- 
+// Déclaration variables de la page édition (utilisateur log)-------------------
 const modeEdition = document.getElementById("bandeauTop");
 const editDesc = document.getElementById("editDesc");
 const editPic = document.getElementById("editPic");
 const editProject = document.getElementById("editProject");
 
-if(token){
-  modeEdition.style.display="block";
-  editDesc.style.display="block";
-  editPic.style.display="block";
-  editProject.style.display="block";
+if (token) {
+  modeEdition.style.display = "block";
+  editDesc.style.display = "block";
+  editPic.style.display = "block";
+  editProject.style.display = "block";
 }
-
 
 // MODALE----------------------------------------------------------------------
 
 const openModale = document.getElementById("editProject");
-const closeModale = document.getElementById("closeModale");
-const modal = document.getElementById("backModale");
-const modalContent = document.getElementById("modale");
+const closeModale = document.querySelectorAll(".closeModale");
+const backModale = document.getElementsByClassName("backModale");
+const backModalModif = document.getElementById("backModalModif");
+const backModalAdd = document.getElementById("backModalAdd");
+const modalContent = document.querySelector(".modale");
 const photoModale = document.getElementById("photoModale");
-
-for(let i = 0; i < listProjects.length; i++){
+const addPic = document.getElementById("addPic");
+const returnModif = document.querySelector(".returnModale");
+//console.log(backModale);
+for (let i = 0; i < listProjects.length; i++) {
   //console.log(listProjects[i].imageUrl);
   const gallery = listProjects[i];
   const unProjet = document.createElement("div");
   const photoProject = document.createElement("img");
+  const trashPic = document.createElement("div");
   photoProject.crossOrigin = "anonymous";
   photoProject.src = gallery.imageUrl;
   unProjet.appendChild(photoProject);
+  unProjet.appendChild(trashPic);
+  trashPic.classList.add("divTrash");
+  const trashCan = document.createElement("i");
+  trashCan.classList.add("fa", "fa-light", "fa-trash-can", "pictoTrash");
+  trashPic.appendChild(trashCan);
+
   /* unProjet.style.cssText = `
   display: flex;
   flex-wrap: wrap;
 	justify-content: space-between;` */
   const titreElement = document.createElement("p");
   titreElement.innerText = "éditer";
-  titreElement.style.textAlign="left";
+  titreElement.style.textAlign = "left";
   unProjet.appendChild(titreElement);
   photoModale.appendChild(unProjet);
 }
-openModale.addEventListener("click", () =>{
+openModale.addEventListener("click", () => {
   //console.log(listProjects);
-  modal.style.display="block";
-})
+  backModalModif.style.display = "block";
+});
 
-/* closeModale.addEventListener("click", () =>{
-  //console.log(listProjects);
-  modal.style.display="none";
-}) */
-closeModale.onclick = function(){
-  modal.style.display="none";
+closeModale.forEach((event) => {
+  event.onclick = function () {
+    backModalModif.style.display = "none";
+    backModalAdd.style.display = "none";
+    document.getElementById("preview").src = "";
+    document.getElementById("fileUpload").style.display = "block";
+    document.querySelector(".picModif").style.display = "block";
+    document.getElementById("ajoutPhotoButton").style.display = "block";
+    document.querySelector(".formatImgButton").style.display = "block";
+    document.getElementById("preview").style.display = "none";
+    document.getElementById("titreAddProjet").value = "";
+    document.getElementById("validPic").style.backgroundColor = "#A7A7A7";
+  };
+});
+window.onclick = function (event) {
+  if (event.target == backModalModif) {
+    backModalModif.style.display = "none";
+    document.getElementById("preview").src = "";
+    document.getElementById("fileUpload").style.display = "block";
+    document.querySelector(".picModif").style.display = "block";
+    document.getElementById("ajoutPhotoButton").style.display = "block";
+    document.querySelector(".formatImgButton").style.display = "block";
+    document.getElementById("preview").style.display = "none";
+    document.getElementById("titreAddProjet").value = "";
+    document.getElementById("validPic").style.backgroundColor = "#A7A7A7";
+  }
+  if (event.target == backModalAdd) {
+    backModalAdd.style.display = "none";
+    document.getElementById("preview").src = "";
+    document.getElementById("fileUpload").style.display = "block";
+    document.querySelector(".picModif").style.display = "block";
+    document.getElementById("ajoutPhotoButton").style.display = "block";
+    document.querySelector(".formatImgButton").style.display = "block";
+    document.getElementById("preview").style.display = "none";
+    document.getElementById("titreAddProjet").value = "";
+    document.getElementById("validPic").style.backgroundColor = "#A7A7A7";
+  }
+};
+
+// AJOUT DU PORJET---------------------------------------------------------
+
+addPic.onclick = function () {
+  backModalAdd.style.display = "block";
+  backModalModif.style.display = "none";
+};
+
+returnModif.onclick = function () {
+  backModalAdd.style.display = "none";
+  backModalModif.style.display = "block";
+  document.getElementById("preview").src = "";
+  document.getElementById("fileUpload").style.display = "block";
+  document.querySelector(".picModif").style.display = "block";
+  document.getElementById("ajoutPhotoButton").style.display = "block";
+  document.querySelector(".formatImgButton").style.display = "block";
+  document.getElementById("preview").style.display = "none";
+  document.getElementById("titreAddProjet").value = "";
+  document.getElementById("validPic").style.backgroundColor = "#A7A7A7";
+};
+
+function displayNone() {
+  document.getElementById("fileUpload").style.display = "none";
+  document.querySelector(".picModif").style.display = "none";
+  document.getElementById("ajoutPhotoButton").style.display = "none";
+  document.querySelector(".formatImgButton").style.display = "none";
+  document.getElementById("preview").style.display = "block";
+  document.getElementById("titreAddProjet").value = "";
+  document.getElementById("validPic").style.backgroundColor = "#A7A7A7";
 }
-window.onclick = function(event){
-  if(event.target == modal){
-    modal.style.display="none";
+function displayBlock() {
+  document.getElementById("fileUpload").style.display = "block";
+  document.querySelector(".picModif").style.display = "block";
+  document.getElementById("ajoutPhotoButton").style.display = "block";
+  document.querySelector(".formatImgButton").style.display = "block";
+  document.getElementById("preview").style.display = "none";
+  document.getElementById("titreAddProjet").value = "";
+  document.getElementById("validPic").style.backgroundColor = "#A7A7A7";
+}
+
+const input = document.getElementById("fileUpload");
+input.addEventListener("change", function () {
+  var oFReader = new FileReader();
+  oFReader.readAsDataURL(input.files[0]);
+  oFReader.onload = function (oFREvent) {
+    document.getElementById("preview").src = oFREvent.target.result;
+    if (document.getElementById("preview").src != "") {
+      displayNone();
+    } else {
+      displayBlock();
+    }
+  };
+});
+
+/* var categorieSet2 = new Set();
+categorieSet2 = categorieSet.delete("Tous"); */
+// console.log(categorieSet);
+for (let uneCategorie of categorieSet) {
+  const optionCategorie = document.createElement("option");
+  if (uneCategorie != "Tous") {
+    optionCategorie.value = uneCategorie;
+    optionCategorie.innerText = uneCategorie;
+    document.getElementById("catAddProjet").appendChild(optionCategorie);
   }
 }
+/* for(let i = 0; i < categorieSet.length; i++){
+  const uneCategorie = document.createElement("option");
+  uneCategorie.value = 
+
+} */
+
+document.getElementById("backModalAdd").addEventListener("change", function () {
+  var myPhoto = document.getElementById("preview").src;
+  var myInput = document.getElementById("titreAddProjet");
+  // console.log(myPhoto);
+  /*   if(myPhoto != null){
+    document.getElementById("titreAddProjet").removeAttribute("disabled");
+  } */
+  if (myInput && myInput.value && myPhoto) {
+    document.getElementById("validPic").style.backgroundColor = "#1D6154";
+  }
+});
+
+// POST NOUVEAU PROJET--------------------------------------------------------
+async function pushProject(event) {
+  event.preventDefault();
+  // var projet = new FormData();
+  // projet.append("image", document.getElementById("preview").src);
+  // projet.append("title", document.getElementById("titreAddProjet").value);
+  // projet.append("category", 1);
+
+  let projet = {
+    image: document.getElementById("preview").src,
+    title: document.getElementById("titreAddProjet").value,
+    // category: document.getElementById("catAddProjet").value,
+    category: 1,
+  };
+
+  console.log("projet", projet);
+  console.log("projetJSON", JSON.stringify(projet));
+  // var token = localStorage.getItem("token");
+
+  // fetch("http://localhost:5678/api/works", {
+  //   method: "POST",
+  //   accept: "application/json",
+  //   headers: {
+  //     Authorization: `Bearer ${token}`,
+  //     "Content-Type": "multipart/form-data",
+  //   },
+  //   body: JSON.stringify(projet),
+  // }).then((response) => {
+  //   if (response.status === 201) {
+  //     //  ajout projet + supp modale
+  //   } else if (response.status === 400) {
+  //     alert("Mauvaise requête"); // exemple format pas bon
+  //   } else if (response.status === 401) {
+  //     alert("Vous n'avez pas les droits");
+  //     console.log(response);
+  //   } else if (response.status === 500) {
+  //     alert("erreur serveur");
+  //     // On transforme la promesse du serv en format JSON
+  //   } else if (!response.ok) {
+  //     const errorMessage = response.text();
+  //     throw new Error(errorMessage);
+  //   }
+  //   return response.json();
+  // });
+  // .then((data) => {
+  //   console.log("data", data);
+  // });
+}
+
+const added = document.getElementById("validPic");
+added.addEventListener("click", pushProject);
+
+// déclenche la fonction "pushProject" au clic (event)
--- FrontEnd/js/script_log.js
+++ FrontEnd/js/script_log.js
@@ -1,42 +1,42 @@
- // PAGE LOGIN
+// PAGE LOGIN
 
-  // Requête avec un temps d'attente (async).
-  async function getInfo(event){
-// Évite d'exécuter l'évènement au chargement de la page   
-    event.preventDefault();
-// Définition des valeurs email et password
-    var email = document.getElementById("email").value
-    var password = document.getElementById("password").value
-// Création d'un objet pour les placer dedans
-    let user = {
-      "email": email,
-      "password": password
-    };
-    // Requête POST vers l'API
-    await fetch('http://localhost:5678/api/users/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(user)
-      // Actions après la réponse du server (.then) -> (response) réponse du serv
-    }).then((response)=>{
-
-        if (response.status === 200){
-          window.location.assign("http://127.0.0.1:5500/FrontEnd/index.html");
-        } else if (response.status === 404){
-            alert("Email ou mot de passe incorrect");
-        } else if (response.status === 401){
-            alert("Email ou mot de passe incorrect");
-        }
-        // On transforme la promesse du serv en format JSON
-        return response.json()
+// Requête avec un temps d'attente (async).
+async function getInfo(event) {
+  // Évite d'exécuter l'évènement au chargement de la page
+  event.preventDefault();
+  // Définition des valeurs email et password
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  // Création d'un objet pour les placer dedans
+  let user = {
+    email: email,
+    password: password,
+  };
+  // Requête POST vers l'API
+  await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(user),
+    // Actions après la réponse du server (.then) -> (response) réponse du serv
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        window.location.assign("http://127.0.0.1:5500/FrontEnd/index.html");
+      } else if (response.status === 404) {
+        alert("Email ou mot de passe incorrect");
+      } else if (response.status === 401) {
+        alert("Email ou mot de passe incorrect");
+      }
+      // On transforme la promesse du serv en format JSON
+      return response.json();
     })
     .then((data) => {
       const token = data.token;
       localStorage.setItem("token", token);
     });
-  }
+}
 /* function login(){
   let login = localStorage.getItem("token");
   if (login){
@@ -45,7 +45,7 @@
     document.getElementById("account").innerText = "login";
   }
 } */
-const form = document.getElementById('login');
+const form = document.getElementById("login");
 // déclenche la fonction "getInfo" au clic (event)
-form.addEventListener('submit', getInfo);
+form.addEventListener("submit", getInfo);
 login();
